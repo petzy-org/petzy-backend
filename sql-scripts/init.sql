a3e07211537989891ae5db8a03fd52f7cfef87c3
Daniel Masegosa <dmasegosadev@gmail.com>
2024-08-12 11:15:46 +0200

    [#4] Create DB schema

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,7 +1,66 @@
-CREATE TABLE animals (
-    id uuid PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type NUMERIC NOT NULL,
-    image_url VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE "animals" (
+    "id" UUID NOT NULL UNIQUE,
+    "pictures" UUID ARRAY,
+    "type" SMALLINT,
+    "shelterId" UUID,
+    "description" VARCHAR,
+    "createdAt" TIMESTAMP,
+    "updatedAt" TIMESTAMP,
+    "available" BOOLEAN,
+    "medicalDetails" VARCHAR ARRAY,
+    "placementType" SMALLINT,
+    "breed" VARCHAR,
+    "preferences" UUID ARRAY,
+    PRIMARY KEY("id")
 );
+
+CREATE TABLE "shelters" (
+    "id" UUID NOT NULL UNIQUE,
+    "name" VARCHAR,
+    "country" VARCHAR,
+    "city" VARCHAR,
+    "coordinates" POINT,
+    -- This is an array
+    "pictures" UUID ARRAY,
+    "verified" BOOLEAN,
+    "description" VARCHAR,
+    "contact" UUID,
+    "donations" VARCHAR ARRAY,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "user" (
+    "id" UUID NOT NULL UNIQUE,
+    "name" VARCHAR,
+    "contact" UUID,
+    "pictures" UUID ARRAY,
+    "description" VARCHAR,
+    "preferences" UUID ARRAY,
+    "type" SMALLINT,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "pictures" (
+    "id" UUID NOT NULL UNIQUE,
+    "url" VARCHAR,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "contact" (
+    "id" UUID NOT NULL UNIQUE,
+    "mail" VARCHAR,
+    "phoneNumber" VARCHAR,
+    "instagramProfile" VARCHAR,
+    "facebookProfile" VARCHAR,
+    PRIMARY KEY("id")
+);
+
+ALTER TABLE "animals"
+ADD FOREIGN KEY("shelterId") REFERENCES "shelters"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "shelters"
+ADD FOREIGN KEY("contact") REFERENCES "contact"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "user"
+ADD FOREIGN KEY("contact") REFERENCES "contact"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
